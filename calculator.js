3204ede3f310af78790c5729dd35357b821f3b2c
Mouad <84838650+modoben98@users.noreply.github.com>
2024-10-16 15:30:05 +0100

    Added test for addition functionality plus code that passes those tests

--- calculator.js
+++ calculator.js
@@ -1,6 +1,12 @@
-const screen = document.querySelector(".screen");
+let firstOperand = 0;
+let secondOperand = 0;
+let result = 0;
+let currentOperation = "";
 
+const screen = document.querySelector(".screen");
 const digitButtons = document.querySelectorAll(".digits button");
+const allClearButton = document.querySelector("#ac");
+const addButton = document.querySelector("#add");
 
 digitButtons.forEach( (digitButton) => {
 	digitButton.addEventListener("click", () => {
@@ -8,8 +14,39 @@ digitButtons.forEach( (digitButton) => {
 	})
 });
 
-const allClearButton = document.querySelector("#ac");
 
 allClearButton.addEventListener("click", () => {
 	screen.textContent = "";
+	currentOperation = "";
+	firstOperand = 0;
+	secondOperand = 0;
+	result = 0;
+});
+
+addButton.addEventListener("click", () => {
+	currentOperation = "addition";
+	
+	if (screen.textContent == "") {
+		result = firstOperand;
+		return ;
+	}
+	firstOperand = Number(screen.textContent);
+	result = firstOperand;
+	screen.textContent = "";
+});
+
+equal.addEventListener("click", () => {
+	secondOperand = Number(screen.textContent);
+
+	if (currentOperation == "addition"){
+		result += secondOperand;
+		currentOperation = "";
+		screen.textContent = result;
+	} else {
+		firstOperand = Number(screen.textContent);
+		result = firstOperand ;
+		screen.textContent = result;
+	}
+
+	
 });
--- calculator.spec.js
+++ calculator.spec.js
@@ -11,12 +11,15 @@ document.body.innerHTML = html;
 
 require('./calculator.js');
 
-describe('screenPrint', () => {
-	
+const screen = document.querySelector(".screen");
+const digitButtons = document.querySelectorAll(".digits button");
+const allClearButton = document.querySelector("#ac");
+const addButton = document.querySelector("#add");
+const equalButton = document.querySelector("#equal");
 
-	test("When digit 7 button is clicked show digit in screen", () => {
-		const screen = document.querySelector(".screen");
-		const digitButton = document.querySelectorAll(".digits button")[0];
+describe('screenPrint', () => {
+	test("When digit 7 button is clicked show digit in screen", () => {		
+		const digitButton = digitButtons[0];
 
 		digitButton.click();
 		
@@ -24,11 +27,9 @@ describe('screenPrint', () => {
 	});
 
 	test("When random digit button is clicked show digit in screen", () => {
-		const screen = document.querySelector(".screen");
 		screen.textContent = "";
-		const digitButtons = document.querySelectorAll(".digits button");
-		const randomIndex = Math.floor(Math.random() * 10);
 
+		const randomIndex = Math.floor(Math.random() * 10);
 		const randomDigitButton = digitButtons[randomIndex] ;
 		
 		randomDigitButton.click();
@@ -38,9 +39,7 @@ describe('screenPrint', () => {
 
 
 	test("When multiple digit buttons are clicked, show digit in screen", () => {
-		const screen = document.querySelector(".screen");
 		screen.textContent = "";
-		const digitButtons = document.querySelectorAll(".digits button");
 		
 		let randomNumber = "";
 
@@ -60,26 +59,16 @@ describe('screenPrint', () => {
 
 describe('allClear', () => {
 	test('when AC button clicked screen stays clear if it is already clear', () => {
-		const screen = document.querySelector(".screen");
-		const allClearButton = document.querySelector("#ac");
-		
 		allClearButton.click();
 
 		expect(screen.textContent).toBe("");
 	});
 
 	test('when AC button clicked after digits were clicked screen clears', () => {
-		const screen = document.querySelector(".screen");
-		const allClearButton = document.querySelector("#ac")
-		const digitButtons = document.querySelectorAll(".digits button");
-
-		let randomNumber = "";
 
 		for (let i = 0; i < 3; i++) {
 			const randomIndex = Math.floor(Math.random() * 10);
 			const randomDigitButton = digitButtons[randomIndex];
-			const randomDigit = randomDigitButton.textContent;
-			randomNumber += randomNumber;
 
 			randomDigitButton.click();
 		}
@@ -89,3 +78,61 @@ describe('allClear', () => {
 		expect(screen.textContent).toBe("");
 	});
 });
+
+describe("add", () => {
+	test("clicking addition button when screen is clear does nothing", () => {
+		allClearButton.click();
+		addButton.click();
+
+		expect(screen.textContent).toBe("");
+	});
+
+	test("clicking add button when screen shows a number clears the screen", () => {		
+		allClearButton.click();
+		for (let i = 0; i < 3; i++) {
+			const randomIndex = Math.floor(Math.random() * 10);
+			const randomDigitButton = digitButtons[randomIndex];
+
+			randomDigitButton.click();
+		}
+
+		addButton.click();
+
+		expect(screen.textContent).toBe("");
+
+	});
+	
+	test("entering a number then clicking addition button then another number and finally equal sign shows the result of the addition in the screen", () => {
+		allClearButton.click();
+		let result = 0;
+
+		let randomNumber = "";
+		for (let i = 0; i < 3; i++) {
+			const randomIndex = Math.floor(Math.random() * 10);
+			const randomDigitButton = digitButtons[randomIndex];
+			const randomDigit = randomDigitButton.textContent;
+			randomNumber += randomDigit;
+
+			randomDigitButton.click();
+		}
+
+		result = Number(randomNumber);
+		addButton.click();
+		
+		randomNumber = "";
+		for (let i = 0; i < 3; i++) {
+			const randomIndex = Math.floor(Math.random() * 10);
+			const randomDigitButton = digitButtons[randomIndex];
+			const randomDigit = randomDigitButton.textContent;
+			randomNumber += randomDigit;
+
+			randomDigitButton.click();
+		}
+
+		result += Number(randomNumber);
+		
+		equalButton.click();
+
+		expect(Number(screen.textContent)).toBe(result);
+	});
+})
